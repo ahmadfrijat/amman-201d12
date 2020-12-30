d7f259648be76c8034d5c1a31f2806b686e43915
ahmadfrijat <ahmadalfrijat7@gmail.com>
2020-12-31 03:50:03 +0400

    problem4 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,8 +8,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b,c) { //eslint-disable-line
     var total = a+b;
+    
     var returnArray = [
         total,
         'The sum of 4 and 7 is 11.'
@@ -32,9 +33,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b,) { //eslint-disable-line
     var multiplyTotal =  a*b;
-
+    
     var returnArray = [
         multiplyTotal,
         "The product of 5 and 9 is 45."
@@ -96,12 +97,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+   var total = 2+3+4;
+   var returnArray = [
+    total,
+    "2,3,4 was passed in as an array of numbers, and 9 is their sum."     
+    ];
+
+    return returnArray;
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
